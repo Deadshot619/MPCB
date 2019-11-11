5af0262a23e3a583a388da12f54fffe229c03fb8
Pankaj <pankaj.makhija@neosofttech.com>
2019-11-11 16:12:22 +0530

    Update Loading Dialog to span full width.

--- app/src/main/java/com/example/mpcb/base/LoadingDialog.kt
+++ app/src/main/java/com/example/mpcb/base/LoadingDialog.kt
@@ -3,11 +3,11 @@ package com.example.mpcb.base
 import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.view.Window
 import androidx.appcompat.app.AppCompatDialog
 import com.example.mpcb.databinding.DialogProgressBinding
-import java.util.*
 
 class LoadingDialog(context: Context) : AppCompatDialog(context) {
 
@@ -19,7 +19,7 @@ class LoadingDialog(context: Context) : AppCompatDialog(context) {
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(dialogProgressBinding.root)
         setCancelable(false)
-        Objects.requireNonNull(window).setLayout(WRAP_CONTENT, WRAP_CONTENT)
+        window!!.setLayout(MATCH_PARENT, WRAP_CONTENT)
     }
 
     fun setMessage(msg: String) {
