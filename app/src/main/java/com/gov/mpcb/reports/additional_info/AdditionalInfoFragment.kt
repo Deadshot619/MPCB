fff35bd0382df06bc2899629164abf4d59bab3b7
Pankaj Makhija <pankaj.makhija@neosofttech.com>
2020-01-31 15:45:27 +0530

    Remove Commented Upload Report File code.

--- app/src/main/java/com/gov/mpcb/reports/additional_info/AdditionalInfoFragment.kt
+++ app/src/main/java/com/gov/mpcb/reports/additional_info/AdditionalInfoFragment.kt
@@ -1,15 +1,9 @@
 package com.gov.mpcb.reports.additional_info
 
 
-import android.Manifest
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.net.Uri
 import android.os.Build
-import android.provider.Settings
 import androidx.annotation.RequiresApi
 import androidx.core.widget.addTextChangedListener
-import com.google.android.material.snackbar.Snackbar
 import com.gov.mpcb.R
 import com.gov.mpcb.base.BaseFragment
 import com.gov.mpcb.databinding.FragmentAdditionalInfoBinding
@@ -21,8 +15,6 @@ import com.gov.mpcb.utils.shared_prefrence.PreferencesHelper.getReportFlagStatus
 import com.gov.mpcb.utils.shared_prefrence.PreferencesHelper.getStringPreference
 import com.gov.mpcb.utils.shared_prefrence.PreferencesHelper.setStringPreference
 import com.gov.mpcb.utils.showMessage
-import com.gov.mpcb.utils.validations.FilePickUtils
-import java.io.File
 
 
 class AdditionalInfoFragment :
@@ -31,19 +23,6 @@ class AdditionalInfoFragment :
 
     private var reports: ReportRequest? = null
     private lateinit var visitReportId: String
-
-     private lateinit var fileP: String
-
-    //File to be uploaded
-//    private val _file: File
-//        get() = File(fileP)
-
-    //File Uri
-    private lateinit var fileUri: Uri
-    //File Path
-    private lateinit var filePath: String
-
-    val PICKFILE_RESULT_CODE = 1000
 
     override fun getLayoutId() = R.layout.fragment_additional_info
     override fun getViewModel() = AdditionalInfoViewModel::class.java
@@ -76,49 +55,6 @@ class AdditionalInfoFragment :
                 setOnClickListener { activity?.finish() }
             }
         }
-
-        //TODO 31/1/2020 : Remove Upload Report functionality
-        /*mBinding.uploadVisitEditTextLayout.setOnClickListener {
-           // showMessage("Clicked!")
-
-
-            if (!PermissionUtils.isStoragePermissionsGranted(ctx = activity!!)){
-
-
-                if (activity!!.checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                    requestPermissions(
-                        PermissionUtils.STORAGE_PERMISSIONS,
-                        100
-                    )
-                } else {
-                    pickFile()
-                }
-
-
-
-        }else{
-                pickFile()
-            }
-        }*/
-    }
-
-    /**
-     * This method creates an intent to choose a file
-     */
-    private fun pickFile(){
-
-        val mimeTypes = arrayOf("image/*", "application/pdf")
-
-        var chooseFile = Intent(Intent.ACTION_GET_CONTENT)
-        chooseFile.type = "image/*|application/pdf"
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            chooseFile.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes)
-        }
-
-        chooseFile = Intent.createChooser(chooseFile, "Choose a file")
-        startActivityForResult(chooseFile, PICKFILE_RESULT_CODE)
-
     }
 
     private fun setListener() {
@@ -150,10 +86,7 @@ class AdditionalInfoFragment :
             if (checkIfReportsFilled()) {
                 mViewModel.submitReport(
                     reportRequest = getReportData(visitReportId)
-                    //TODO 31/1/2020 : Remove Upload Report functionality
-//                    file = File(fileP)
                 )
-//                mViewModel.uploadVisitFile(File(fileP))
             } else {
                 showMessage("Please fill all the reports!")
             }
@@ -184,14 +117,10 @@ class AdditionalInfoFragment :
             showMessage("Select Unit Compiled")
             return false
         }
-        //TODO 31/1/2020 : Remove Upload Report functionality
-        /*if (mBinding.uploadVisitEditTextLayout.text.isNullOrEmpty()) {
-            showMessage("Select a File")
-            return false
-        }*/
         return true
     }
 
+    //When report is submitted successful, return to Visit Report page
     override fun onSubmitReportSuccess(msg: String) {
         showMessage(msg)
         activity!!.finish()
@@ -233,10 +162,6 @@ class AdditionalInfoFragment :
                         rgUnitComplied.check(R.id.rbUnitYes)
                     else
                         rgUnitComplied.check(R.id.rbUnitNo)
-
-                    //TODO 31/1/2020 : Remove Upload Report functionality
-/*                    if (visitReportFile.isNotEmpty())
-                        mBinding.uploadVisitEditTextLayout.setText(visitReportFile)*/
                 }
             }
         }
@@ -247,89 +172,4 @@ class AdditionalInfoFragment :
 
         setDataToViews()
     }
-
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-
-
-        when (requestCode) {
-            PICKFILE_RESULT_CODE -> if (resultCode == -1) {
-                //File Uri
-                fileUri = data?.data!!
-                //File Path
-             //   getRealPathFromURI(activity)
-
-                fileP =
-                    if (FilePickUtils.getSmartFilePath(activity!!,fileUri) == null)
-                        ""
-                    else
-                        FilePickUtils.getSmartFilePath(activity!!,fileUri).toString()
-
-                filePath = fileUri.path!!
-                mBinding.uploadVisitEditTextLayout.setText(File(fileP).name)
-
-              //  val visitReportBodyLocal = RequestBody.create(MediaType.parse("image/*"), _file.absoluteFile)
-//                val visitReportBodyLocal = RequestBody.create(MediaType.parse("image/*"),fileP)
-//                val visitReportPartLocal =
-//                    MultipartBody.Part.createFormData("file", _file.name, visitReportBodyLocal)
-            }
-        }
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray
-    ) {
-        when (requestCode) {
-            100 -> {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.isNotEmpty()
-                    && grantResults[0] == PackageManager.PERMISSION_GRANTED
-                ) {
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
-                    pickFile()
-                } else { // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-
-                    // user rejected the permission
-                    val showRationale= shouldShowRequestPermissionRationale( Manifest.permission.READ_EXTERNAL_STORAGE)
-                    if (! showRationale) {
-                        // user also CHECKED "never ask again"
-                        // you can either enable some fall back,
-                        // disable features of your app
-                        // or open another dialog explaining
-                        // again the permission and directing to
-                        // the app setting
-                        Snackbar.make(
-                            mBinding.root,
-                            "Grant Storage permission to continue!",
-                            Snackbar.LENGTH_LONG
-                        )
-                            .setAction("Open"){
-                                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
-                                val uri = Uri.fromParts("package", context?.packageName, null);
-                                intent.data = uri
-                                startActivity(intent)
-                            }
-                            .show()
-
-                    } else {
-                        showMessage("You need to accept this permission to continue!")
-                    }
-                }
-                return
-            }
-        }
-    }
-
-
-
-
-
-
-
-
-
 }
--- app/src/main/java/com/gov/mpcb/reports/additional_info/AdditionalInfoViewModel.kt
+++ app/src/main/java/com/gov/mpcb/reports/additional_info/AdditionalInfoViewModel.kt
@@ -8,10 +8,6 @@ import com.gov.mpcb.network.response.LoginResponse
 import com.gov.mpcb.utils.constants.Constants
 import com.gov.mpcb.utils.shared_prefrence.PreferencesHelper
 import io.reactivex.functions.Consumer
-import okhttp3.MediaType
-import okhttp3.MultipartBody
-import okhttp3.RequestBody
-import java.io.File
 
 
 class AdditionalInfoViewModel : BaseViewModel<AdditionalInfoNavigator>() {
@@ -24,8 +20,7 @@ class AdditionalInfoViewModel : BaseViewModel<AdditionalInfoNavigator>() {
     private val indusImisId = PreferencesHelper.getStringPreference(Constants.INDUS_IMIS_ID, "")
 
     fun submitReport(
-        reportRequest: ReportRequest?,
-        file: File? = null
+        reportRequest: ReportRequest?
     ) {
 //        val reportData = PreferencesHelper.getStringPreference(Constants.REPORT_KEY, "")
 
@@ -41,10 +36,8 @@ class AdditionalInfoViewModel : BaseViewModel<AdditionalInfoNavigator>() {
                 reportRequest,
                 Consumer {
                     dialogVisibility.value = false
-//                    mNavigator!!.onSubmitReportSuccess(it.message)
                     //If report submitted successfully, update the list to show status as visited
                     if (it.status) {
-//                        uploadVisitFile(file)
                         PreferencesHelper.setBooleanPreference(Constants.FORM_COMPLETE_STATUS, true)
                         mNavigator!!.onSubmitReportSuccess(it.message)
                     }
@@ -54,51 +47,5 @@ class AdditionalInfoViewModel : BaseViewModel<AdditionalInfoNavigator>() {
                 })
             )
         }
-    }
-
-    //TODO 31/1/2020 : Remove Upload Report functionality
-    fun uploadVisitFile(file: File) {
-        //Make a RequestBody of each value to be sent.
-
-        //File
-        val visitReportBodyLocal: RequestBody
-        val visitReportPartLocal: MultipartBody.Part
-
-        val requestIdLocal = RequestBody.create(
-            MediaType.parse("text/plain"), ""
-        )
-        val userIdLocal = RequestBody.create(
-            MediaType.parse("text/plain"), user.userId.toString()
-        )
-        val visitIdLocal = RequestBody.create(
-            MediaType.parse("text/plain"), visitId.toString()
-        )
-        val industryImisIdLocal = RequestBody.create(
-            MediaType.parse("text/plain"), indusImisId.toString()
-        )
-
-        visitReportBodyLocal = RequestBody.create(MediaType.parse("image/pdf/*"), file)
-        visitReportPartLocal =
-            MultipartBody.Part.createFormData("file", file.name, visitReportBodyLocal)
-
-        dialogMessage.value = "Uploading File..."
-        dialogVisibility.value = true
-
-        mDisposable.add(DataProvider.uploadVisitReportFile(
-            requestId = requestIdLocal,
-            userId = userIdLocal,
-            visitId = visitIdLocal,
-            indusImisId = industryImisIdLocal,
-            visitReportFile = visitReportPartLocal,
-            success = Consumer {
-                dialogVisibility.value = false
-                //If report submitted successfully, update the list to show status as visited
-                if (it.status) {
-                    PreferencesHelper.setBooleanPreference(Constants.FORM_COMPLETE_STATUS, true)
-                }
-//                mNavigator!!.onSubmitReportSuccess(it.message)
-            },
-            error = Consumer { checkError(it) })
-        )
     }
 }
