d56b277f7ef0da809eea5df99404a48fc45b0023
Pankaj Makhija <pankaj.makhija@neosofttech.com>
2020-03-11 18:15:22 +0530

    Call viewAppliedList APi successfully.
    - Comment code causing parsing error in APi.

--- app/src/main/java/com/gov/mpcb/menu_tabs/surprise_inspections/applied_by_me/AppliedByMeFragment.kt
+++ app/src/main/java/com/gov/mpcb/menu_tabs/surprise_inspections/applied_by_me/AppliedByMeFragment.kt
@@ -1,15 +1,22 @@
 package com.gov.mpcb.menu_tabs.surprise_inspections.applied_by_me
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.gov.mpcb.R
-import com.gov.mpcb.base.BaseFragment
 import com.gov.mpcb.databinding.FragmentAppliedByMeBinding
+import com.gov.mpcb.menu_tabs.surprise_inspections.SurpriseInspectionsViewModel
 import com.gov.mpcb.utils.showMessage
 
 /**
  * A simple [Fragment] subclass.
  */
-class AppliedByMeFragment : BaseFragment<FragmentAppliedByMeBinding, AppliedByMeViewModel>(),
+/*class AppliedByMeFragment : BaseFragment<FragmentAppliedByMeBinding, AppliedByMeViewModel>(),
 AppliedByMeNavigator{
 
     override fun getLayoutId() = R.layout.fragment_applied_by_me
@@ -27,5 +34,34 @@ AppliedByMeNavigator{
 
     }
 
+}*/
+
+class AppliedByMeFragment : Fragment(){
+
+    private lateinit var mBinding : FragmentAppliedByMeBinding
+    private lateinit var mViewModel: SurpriseInspectionsViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_applied_by_me, container,false)
+
+        return mBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        mViewModel = activity?.run {
+            ViewModelProvider(this).get(SurpriseInspectionsViewModel::class.java)
+        }!!
+
+        mViewModel._viewAppliedLists.observe(viewLifecycleOwner, Observer {
+            if (it.data.isNotEmpty())
+                showMessage("${it.total_rows}")
+            else
+                showMessage("${it.total_rows}")
+        })
+    }
 
 }
